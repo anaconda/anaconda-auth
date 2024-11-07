66488df1f94049dadd6b35f3f9352df6b1492467
Matt Kramer <mkramer@anaconda.com>
2024-11-06 19:13:17 -0600

    Fix auth-handler plugin by making sure the entire module is imported (#511)

--- libs/anaconda-cloud-auth/src/anaconda_cloud_auth/_conda/entrypoint.py
+++ libs/anaconda-cloud-auth/src/anaconda_cloud_auth/_conda/entrypoint.py
@@ -3,6 +3,6 @@
 # Plugins were first introduced in conda=22.11.0
 # The ChannelAuthBase class was introduced in conda=23.7.0
 try:
-    from anaconda_cloud_auth._conda import plugins  # noqa: F401
+    from anaconda_cloud_auth._conda.plugins import *  # noqa: F403
 except ImportError:
     pass
