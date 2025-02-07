d9d0fa2ff9a0f007fd23c22c41f01a8cc7508739
mattkram <mkramer@anaconda.com>
2025-02-07 15:40:14 -0600

    fix: Test fixture to prevent breaking the dev environment

--- tests/conda_token/conftest.py
+++ tests/conda_token/conftest.py
@@ -102,16 +102,20 @@ def secret_token():
 
 @pytest.fixture(scope="function")
 def uninstall_rope():
-    run_command(Commands.REMOVE, "rope", "-y", use_exception_handler=True)
+    run_command(Commands.REMOVE, "rope", "-y", "--force", use_exception_handler=True)
     yield
-    run_command(Commands.REMOVE, "rope", "-y", use_exception_handler=True)
+    run_command(Commands.REMOVE, "rope", "-y", "--force", use_exception_handler=True)
 
 
 @pytest.fixture(scope="function")
 def uninstall_colorama():
-    run_command(Commands.REMOVE, "colorama", "-y", use_exception_handler=True)
+    run_command(
+        Commands.REMOVE, "colorama", "-y", "--force", use_exception_handler=True
+    )
     yield
-    run_command(Commands.REMOVE, "colorama", "-y", use_exception_handler=True)
+    run_command(
+        Commands.REMOVE, "colorama", "-y", "--force", use_exception_handler=True
+    )
 
 
 @pytest.fixture
