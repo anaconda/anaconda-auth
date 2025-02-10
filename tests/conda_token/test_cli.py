1f792dfaee4868bcc85bf569940ce4ff89dc4c49
mattkram <mkramer@anaconda.com>
2025-02-10 11:07:59 -0600

    refac: Rename cli.py -> conda_token.py

--- tests/conda_token/test_cli.py
+++ tests/conda_token/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 import urllib3.exceptions
 from packaging.version import parse
 
-from anaconda_auth._conda import cli
+from anaconda_auth._conda.conda_token import cli
 from anaconda_auth._conda.repo_config import CONDA_VERSION
 from anaconda_auth._conda.repo_config import CondaVersionWarning
 
@@ -10,21 +10,21 @@ from anaconda_auth._conda.repo_config import CondaVersionWarning
 def test_token_set_no_verify_ssl(remove_anaconda_cloud_token, secret_token, capsys):
     # real InsecureRequestWarning against real server
     with pytest.warns(urllib3.exceptions.InsecureRequestWarning):
-        cli.cli(["set", "--no-ssl-verify", secret_token])
+        cli(["set", "--no-ssl-verify", secret_token])
 
 
 def test_token_set_no_verify_ssl_mock_server(
     remove_token, secret_token, capsys, repo_url
 ):
-    cli.cli(["set", "--no-ssl-verify", secret_token])
-    ret = cli.cli(["list"])
+    cli(["set", "--no-ssl-verify", secret_token])
+    ret = cli(["list"])
     assert ret == 0
     captured = capsys.readouterr()
     assert captured.out.splitlines()[-1] == repo_url + " " + secret_token
 
 
 def test_token_list(remove_token, capsys, repo_url):
-    ret = cli.cli(["list"])
+    ret = cli(["list"])
     captured = capsys.readouterr()
     assert ret == 1
     assert (
@@ -35,13 +35,13 @@ def test_token_list(remove_token, capsys, repo_url):
 
 def test_token_set_invalid_channel(remove_token):
     with pytest.raises(SystemExit):
-        cli.cli(["set", "secret", "--include-archive-channels", "nope"])
+        cli(["set", "secret", "--include-archive-channels", "nope"])
 
 
 def test_token_set(remove_token, secret_token, capsys, repo_url):
-    cli.cli(["set", secret_token])
+    cli(["set", secret_token])
 
-    ret = cli.cli(["list"])
+    ret = cli(["list"])
     assert ret == 0
     captured = capsys.readouterr()
     assert (
@@ -52,7 +52,7 @@ def test_token_set(remove_token, secret_token, capsys, repo_url):
 
 
 def test_token_set_error(remove_token, capsys, repo_url):
-    ret = cli.cli(["set", "SECRET"])
+    ret = cli(["set", "SECRET"])
     assert ret == 1
     captured = capsys.readouterr()
     # we will also capture a HTTP log for the mock server; check the last line.
@@ -61,7 +61,7 @@ def test_token_set_error(remove_token, capsys, repo_url):
         == "The token could not be validated. Please check that you have typed it correctly."
     )
 
-    ret = cli.cli(["list"])
+    ret = cli(["list"])
     assert ret == 1
     captured = capsys.readouterr()
     assert (
@@ -76,5 +76,5 @@ def test_token_set_error(remove_token, capsys, repo_url):
 )
 def test_token_set_with_signing_warn(remove_token, secret_token, capsys):
     with pytest.warns(CondaVersionWarning):
-        ret = cli.cli(["set", "--enable-signature-verification", secret_token])
+        ret = cli(["set", "--enable-signature-verification", secret_token])
         assert ret == 0
