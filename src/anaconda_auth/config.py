a7b2202698ac46680a7e17c35c225394eb5d7c3f
mattkram <mkramer@anaconda.com>
2025-02-18 22:48:26 -0600

    refac: Move implementation of AnacondaCloudConfig into anaconda_auth

--- src/anaconda_auth/config.py
+++ src/anaconda_auth/config.py
@@ -7,6 +7,7 @@ from typing import Union
 
 import requests
 from pydantic import BaseModel
+from pydantic_settings import SettingsConfigDict
 
 from anaconda_auth import __version__ as version
 from anaconda_cli_base.config import AnacondaBaseSettings
@@ -14,9 +15,6 @@ from anaconda_cli_base.config import AnacondaBaseSettings
 
 class AnacondaAuthConfig(AnacondaBaseSettings, plugin_name="auth"):
     def __init__(self, **kwargs: Any):
-        # TODO: Remove circular import and move base definition into this file
-        from anaconda_cloud_auth.config import AnacondaCloudConfig
-
         if self.__class__ == AnacondaAuthConfig:
             config = AnacondaCloudConfig()
             set_fields = config.model_dump(exclude_unset=True)
@@ -70,3 +68,25 @@ class AnacondaAuthConfig(AnacondaBaseSettings, plugin_name="auth"):
 class OpenIDConfiguration(BaseModel):
     authorization_endpoint: str
     token_endpoint: str
+
+
+_OLD_OIDC_REQUEST_HEADERS = {"User-Agent": f"anaconda-cloud-auth/{version}"}
+
+
+class AnacondaCloudConfig(AnacondaAuthConfig, plugin_name="cloud"):
+    # Here, we explicitly specify the model_config for this class. This is because
+    # there is a bug inside AnacondaBaseSettings, where the env_prefix is mutated
+    # in that base class. Thus, nested inheritance doesn't quite work as I'd expect.
+    # However, if we set this attribute on *this* class, then that problem goes away,
+    # Even though the behavior that handles the injecting of the `plugin_name` into
+    # the env_prefix is handled in the __init_subclass__ method in that base class.
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        pyproject_toml_table_header=(),
+        env_prefix="ANACONDA_",
+        env_nested_delimiter="__",
+        extra="ignore",
+        ignored_types=(cached_property,),
+    )
+    domain: str = "anaconda.cloud"
+    oidc_request_headers: Dict[str, str] = _OLD_OIDC_REQUEST_HEADERS
