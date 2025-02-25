a64ea729d4600b415721276de206cbd57737940b
mattkram <mkramer@anaconda.com>
2025-02-25 15:18:17 -0600

    tidy: Change ordering of functions

--- src/anaconda_auth/repo.py
+++ src/anaconda_auth/repo.py
@@ -77,6 +77,18 @@ def _set_repo_token(org_name: str, token: str) -> None:
     token_info.save()
 
 
+def _print_repo_token_table(tokens: dict[str, str]) -> None:
+    table = Table(title="Anaconda Repository Tokens", title_style="green")
+
+    table.add_column("Channel URL")
+    table.add_column("Token")
+
+    for url, token in tokens.items():
+        table.add_row(url, token)
+
+    console.print(table)
+
+
 @app.callback(invoke_without_command=True, no_args_is_help=True)
 def main() -> None:
     """Manage your Anaconda repo tokens."""
@@ -93,18 +105,6 @@ def list_tokens() -> None:
         raise typer.Abort()
 
     _print_repo_token_table(tokens)
-
-
-def _print_repo_token_table(tokens: dict[str, str]) -> None:
-    table = Table(title="Anaconda Repository Tokens", title_style="green")
-
-    table.add_column("Channel URL")
-    table.add_column("Token")
-
-    for url, token in tokens.items():
-        table.add_row(url, token)
-
-    console.print(table)
 
 
 @app.command(name="install")
