6b5ae8fc7cbcb1d93b2d8852c04c38174bb6f044
mattkram <mkramer@anaconda.com>
2025-03-13 22:06:36 -0500

    fix: Add another import for TokenInfo

--- src/anaconda_cloud_auth/client.py
+++ src/anaconda_cloud_auth/client.py
@@ -1,7 +1,10 @@
 from anaconda_auth.client import *  # noqa: F403
-from anaconda_cloud_auth import warn  # noqa: F401
 
-# The following import addresses a bug in Navigator, which imported from the wrong module by mistake
-from anaconda_cloud_auth.config import AnacondaCloudConfig  # noqa: F401
+# The following imports address a bug in Navigator, which imported from the wrong module by mistake
+from anaconda_auth.config import AnacondaCloudConfig  # noqa: F401
+from anaconda_auth.token import TokenInfo  # noqa: F401
+
+# This one needs to stay to raise the deprecation warning
+from anaconda_cloud_auth import warn  # noqa: F401
 
 warn()
