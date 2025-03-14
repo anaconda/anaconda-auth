59f13071b0913d453427a90c42691f2f2dc11bc9
mattkram <mkramer@anaconda.com>
2025-03-13 21:46:22 -0500

    fix: Add additional import alias to address Navigator bug

--- src/anaconda_cloud_auth/client.py
+++ src/anaconda_cloud_auth/client.py
@@ -1,4 +1,7 @@
 from anaconda_auth.client import *  # noqa: F403
 from anaconda_cloud_auth import warn  # noqa: F401
 
+# The following import addresses a bug in Navigator, which imported from the wrong module by mistake
+from anaconda_cloud_auth.config import AnacondaCloudConfig  # noqa: F401
+
 warn()
