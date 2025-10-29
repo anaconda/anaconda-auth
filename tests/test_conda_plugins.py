ec8acb47e7e046710227e39fbaea599679f9744f
Michael C. Grant <mcg@cvxr.com>
2025-10-29 12:00:48 -0500

    Soft fail missing anaconda tokens (#153)
    * soft fail missing anaconda tokens
    
    * chore(docs): Update docstring
    
    * feat: Add a test
    
    * fix: Add explicit return
    
    * feat: Handle both 401 and 403 errors
    
    * fix: Update test for new behavior of _load_token
    
    ---------
    
    Co-authored-by: mattkram <mkramer@anaconda.com>

--- tests/test_conda_plugins.py
+++ tests/test_conda_plugins.py
@@ -139,10 +139,10 @@ def test_get_token_for_main_finds_first_token(handler):
 
 @pytest.mark.usefixtures("mocked_empty_conda_token")
 def test_get_token_missing(handler):
-    with pytest.raises(AnacondaAuthError):
-        _ = handler._load_token(
-            "https://repo.anaconda.cloud/repo/my-org/my-channel/noarch/repodata.json"
-        )
+    token = handler._load_token(
+        "https://repo.anaconda.cloud/repo/my-org/my-channel/noarch/repodata.json"
+    )
+    assert token is None
 
 
 @pytest.fixture()
@@ -175,16 +175,51 @@ def test_inject_header_during_request(session, url, monkeypatch):
     assert request.headers.get("Authorization") == "token my-test-token-in-token-info"
 
 
+@pytest.mark.parametrize("mocked_status_code", [401, 403])
 @pytest.mark.usefixtures("mocked_token_info")
-def test_response_callback_403(session, url, monkeypatch):
-    def trigger_403(req, *args, **kwargs):
+def test_response_callback_error_handler(
+    mocked_status_code, *, session, url, monkeypatch
+):
+    def _mocked_request(req, *args, **kwargs):
         response = Response()
-        response.status_code = 403
+        response.status_code = mocked_status_code
         response = dispatch_hook("response", req.hooks, response, **kwargs)
         return response
 
-    monkeypatch.setattr(session, "send", trigger_403)
+    monkeypatch.setattr(session, "send", _mocked_request)
 
     # A 403 response is captured by the hook and a custom exception is raised
     with pytest.raises(AnacondaAuthError):
         session.get(url)
+
+
+@pytest.mark.parametrize("mocked_status_code", [401, 403])
+def test_inject_no_header_during_request_if_no_token(
+    mocked_status_code, *, session, url, monkeypatch
+):
+    """
+    If there is not token, we first make a request without an Authorization header.
+    If the server responds with an error code, we raise an exception.
+    """
+    # Set up a dummy function that will capture the PreparedRequest without sending it.
+    request = None
+
+    def _mocked_request(req, *args, **kwargs):
+        # Capture the request object for introspection later
+        nonlocal request
+        request = req
+
+        # Simulate a 403 response from the server
+        response = Response()
+        response.status_code = mocked_status_code
+        response = dispatch_hook("response", req.hooks, response, **kwargs)
+        return response
+
+    monkeypatch.setattr(session, "send", _mocked_request)
+
+    # An error response is captured by the hook and a custom exception is raised
+    with pytest.raises(AnacondaAuthError):
+        session.get(url)
+
+    # Make sure the token did not get injected
+    assert request.headers.get("Authorization") is None
